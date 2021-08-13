bd01980d6f56f5061301f0b02a18b1d2536589e3
connor-zh <connor-zh@r-1-101-25-172.comp.nus.edu.sg>
2021-08-13 16:22:01 +0800

    update gitignore

--- redis-6.2.5/src/quicklist.d
+++
@@ -1,2 +0,0 @@
-quicklist.o: quicklist.c quicklist.h zmalloc.h config.h ziplist.h util.h \
-  sds.h lzf.h redisassert.h
--- redis-6.2.5/src/release.h
+++
@@ -1,3 +0,0 @@
-#define REDIS_GIT_SHA1 "1c785b1c"
-#define REDIS_GIT_DIRTY "       0"
-#define REDIS_BUILD_ID "connor-zhdeAir-1628786484"
